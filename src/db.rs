0ad962a308de3e35d92ea6f1e496c9a2dc4fe195
Riccardo Ripanti <riccardo.ripanti01@gmail.com>
2026-03-10 12:45:08 +0100

    Authorize users with /etc/shadow
    Now when a new user logs-in, the user is created and it's password is
    stored in /etc/shadow using the SHA256 algorithm.
    
    When an existing user tries to log-in the relative entry in /etc/shadow
    is checked to see if the hash of the provided password matches.

--- src/db.rs
+++ src/db.rs
@@ -1,9 +1,15 @@
 use std::net::SocketAddr;
 use std::sync::{Arc, Mutex};
 
+use tokio::process::Command;
+
+use sha2::{Digest, Sha256};
+
+use base64::prelude::*;
+
 struct Entry {
     addr: Option<SocketAddr>,
-    user: String,
+    user: uzers::User,
 }
 
 #[derive(Clone, Default)]
@@ -17,31 +23,67 @@ impl Database {
         Default::default()
     }
 
-    pub fn authorized(&self, addr: SocketAddr, user: &str, password: &str) -> anyhow::Result<()> {
+    /// Hashes the password as it would be done by the `crypt` command
+    fn crypt(password: &str) -> String {
+        //↓ 5 is the code for SHA256
+        format!("$5$${}", BASE64_STANDARD.encode(Sha256::digest(password)))
+    }
+
+    /// Add the user to the OS and set its password in /etc/shadow
+    async fn add(user: &str, password: &str) -> anyhow::Result<()> {
+        if !Command::new("useradd")
+            .arg("-m")
+            .arg(user)
+            .status()
+            .await?
+            .success()
+        {
+            anyhow::bail!("useradd failed")
+        };
+
+        let password = Self::crypt(password);
+
+        let shadow = tokio::fs::read_to_string("/etc/shadow").await?;
+        let shadow = shadow.replace(&format!("{user}:!:"), &format!("{user}:{password}:"));
+        tokio::fs::write("/etc/shadow", shadow).await?;
+
+        Ok(())
+    }
+
+    pub async fn authorize(
+        &self,
+        addr: SocketAddr,
+        user: &str,
+        password: &str,
+    ) -> anyhow::Result<()> {
         let user = user.trim();
         if user == "root" {
             anyhow::bail!("root login is not allowed!")
         }
 
+        let Some(uzer) = uzers::get_user_by_name(user) else {
+            return Self::add(user, password).await;
+        };
+
+        let password = format!("{user}:{}:", Self::crypt(password));
+        let shadow = tokio::fs::read_to_string("/etc/shadow").await?;
+        if !shadow.lines().any(|line| line.starts_with(&password)) {
+            anyhow::bail!("Wrong password!")
+        }
+
         let mut lock = self.inner.lock().unwrap();
-        match lock.iter().position(|e| &e.user == user) {
-            None => {
-                lock.push(Entry {
-                    addr: Some(addr),
-                    user: user.to_string(),
-                    pswd: password.to_string(),
-                });
-            }
+        match lock.iter().position(|e| e.user.name() == uzer.name()) {
+            None => lock.push(Entry {
+                addr: Some(addr),
+                user: uzer,
+            }),
             Some(pos) => {
                 if let Some(addr) = lock[pos].addr {
                     anyhow::bail!(
                         "Rejecting new connection from {user}: already logged-in from {addr}",
                     );
                 }
-                if &lock[pos].pswd != password {
-                    anyhow::bail!("Rejecting new connection from {user}: worng credentials",);
-                }
-                lock[pos].addr = Some(addr);
+                lock[pos].addr = Some(addr)
             }
         }
         Ok(())
@@ -54,7 +96,7 @@ impl Database {
         };
     }
 
-    pub fn user(&self, addr: SocketAddr) -> Option<String> {
+    pub fn user(&self, addr: SocketAddr) -> Option<uzers::User> {
         let lock = self.inner.lock().unwrap();
         lock.iter()
             .position(|e| e.addr == Some(addr))
