86c36ad4a65e8f36656f447161f11912abab51ac
Riccardo Ripanti <riccardo.ripanti01@gmail.com>
2026-03-22 13:40:38 +0100

    temp

--- build.rs
+++ build.rs
@@ -2,15 +2,18 @@ use std::{env, path::PathBuf};
 
 fn main() {
     println!("cargo:rustc-link-lib=crypt");
+    println!("cargo:rustc-link-lib=pam");
 
-    let bindings = bindgen::Builder::default()
-        .header("src/crypt.h")
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
-        .generate()
-        .expect("Unable to generate bindings");
+    for header in ["crypt", "pam_appl"] {
+        let bindings = bindgen::Builder::default()
+            .header(format!("src/{header}.h"))
+            .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+            .generate()
+            .expect("Unable to generate bindings");
 
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    bindings
-        .write_to_file(out_path.join("bindings.rs"))
-        .expect("Couldn't write bindings!");
+        let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+        bindings
+            .write_to_file(out_path.join(format!("{header}.rs")))
+            .expect("Couldn't write bindings!");
+    }
 }
